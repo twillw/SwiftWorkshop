22cee188f0f05f0ee9ab6b70656a9e3f2a53dddc
Tom Wark <tom@tomwark.me>
2015-11-21 14:03:20 -0500

    Initial Commit

---
+++ NascentWorkshop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NascentWorkshop
+//
+//  Created by Tom Wark on 2015-11-21.
+//  Copyright © 2015 Tom Wark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
